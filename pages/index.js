c53e5a24fa4ab624647ee3b3a1ba1af1d434052e
Italo Sol dos Santos Dias <italosol200@hotmail.com>
2021-01-26 20:33:18 -0300

    commit com as coisas ajustadas

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,74 @@
 import { Children } from 'react'
 import styled from 'styled-components'
-import db from '../db.json';
+import db from '../db.json'
 
 
-const BackgroundImage = styled.div ` 
-background-image: url(${db.bg});
-  flex: 1;
-  background-size: cover;
-  background-position: center;
-  `;
+import RetangulosDoQuiz from '../src/componentes/retangulo/retangulo.js'
+import FundoDoQuiz from '../src/componentes/FundoTela/fundoDoQuiz.js'
 
- export const QuizContainer = styled.div`
-    width: 100%;
-    max-width: 350px;
-    margin: auto 10%;
-    padding-top: 45px;
-    @media screen and (max-width: 500px){
-      magin: auto;
-      padding: 15px
-    }  
- `;
-// define as propriedades dos retangulos que ficam dentro do container quiz//
- const RetangulosDoQuiz = styled.div`
-    margin-top : 24px;
-    margin-bottom: 24px;
-    border: 1px solid (${db.primary});
-    background-color: #1c1814;
-    border-radius: 4px;
-    overflow: hidden;
+import Footer from '../src/componentes/Footer/index.js'
+import GitHubCorner from '../src/componentes/GithubCorner/index.js';
+
+// const BackgroundImage = styled.div ` 
+// background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+//   `;
+
+//  export const RetangulosDoQuiz = styled.div`
+//     width: 100%;
+//     max-width: 350px;
+//     margin: auto 10%;
+//     padding-top: 45px;
+//     @media screen and (max-width: 500px){
+//       magin: auto;
+//       padding: 15px;
+//     }  
+//  `;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `;
-// function Title (props) { // <==propriedades
-//   return ( <h1> 
-//     {props.Children} </h1>
-//   )
-// }
-
 
 export default function Home() {
+
   return(
-    <BackgroundImage>
+    <FundoDoQuiz backgroundImage= {db.bg}>
      <QuizContainer>
+
        <RetangulosDoQuiz>
-         
-      vai vendo
+
+         {/* esse abaixo é o cabeçalho do quiz */}
+         <RetangulosDoQuiz.Header>
+
+     <h1> Pokemon</h1>
+         </RetangulosDoQuiz.Header>
+
+         {/* esse abaixo é o conteudo do retangulo */}
+         <RetangulosDoQuiz.Content>
+        <p>lorem bla bla bla</p>
+         </RetangulosDoQuiz.Content>
        </RetangulosDoQuiz>
+      
+      <RetangulosDoQuiz>
+        {/* esse abaixo é o conteudo do retangulo */}
+       <RetangulosDoQuiz.Content>
+         <h1> Quizis do Git Hub</h1>
+
+         <p>lorem bla bla bla</p>
+       </RetangulosDoQuiz.Content>
+      </RetangulosDoQuiz>
+      <Footer />
      </QuizContainer>
-    </BackgroundImage>
+     <GitHubCorner projectUrl="" />
+    </FundoDoQuiz>
   );
 }
